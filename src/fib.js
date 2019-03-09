d55baf92e6a15f3d9a929b2ffa85d9f9f0f86e25
Arianna Bunnell <ariannabunnell@gmail.com>
2019-03-09 15:55:52 -0700

    housekeeping

--- src/fib.js
+++ src/fib.js
@@ -1,44 +1,13 @@
-// Re-set the document title
-document.title = 'TODO: Dynamic Fibonacci Sequence in JavaScript';
 
-// Create a red div in the body of the document
+document.title = 'Dynamic Fibonacci Sequence in JavaScript';
+
 var div = document.createElement('div');
 div.setAttribute('class', 'red fib-container');
 document.querySelector('body').appendChild(div);
 
-// Make a paragraph to hold some instructions
-/*var para = document.createElement('p');
-para.textContent = "Write code necessary to";
-div.appendChild(para);*/
-
 var para = document.createElement('p');
 para.textContent = "Slider for Fibbonacci Sequence ";
 div.appendChild(para);
-
-/*class divFib {
-    divFib(depth, fib) {
-        this.fib = fib;
-        this.Fdiv = document.createElement('div');
-        Fdiv.setAttribute('class', 'fib-item');
-        this.depth = depth;
-    }
-}
-
-{
-    div: 1,
-    val: 1
-}.div
-*/
-
-
-/*    <form id="list-of-divs">
-        <label for="list-slider" id="list-label">List(0)</label>
-        <input id="list-slider" type="range"
-               min="0" max="50" value="0"
-               oninput="listSlider(this)"
-        />
-    </form>*/
-
 
 var f = document.createElement('form');
 f.setAttribute('id', 'list-of-divs');
@@ -64,12 +33,6 @@ div2.setAttribute('class', 'fib-container');
 div2.setAttribute('id', 'tree-of-divs');
 div.appendChild(div2);
 
-/*<div id="tree-of-divs" class="fib-container">
-        <div class="fib-item">
-            <p>0</p>
-        </div>
-    </div>*/
-
 var newDiv = document.createElement('div');
 newDiv.setAttribute('class', 'fib-item');
 
@@ -78,44 +41,7 @@ p.textContent = 'Fib(0) = 0';
 newDiv.appendChild(p);
 
 div2.appendChild(newDiv);
-/*var fib = function(val) {
-    if (val == 0 ) {
-        return 0;
-    }
-    if (val == 1 ) {
-        return 1;
-    }
-    if (val == 2 ) {
-        return 1;
-    }
-    return fib(val-1) + fib(val-2)
-}
 
-var recursiveBinTree = function(divFib) {
-    //this should all be part of the class
-    //moved the creation of the div to the fibDiv class
-  /*  var newP = document.createElement('p');
-    newP.textContent = `Fib(${depth}) = ${depth}`
-    newDiv.appendChild(newP);*/
-
-        /*var left = recursiveBinTree(new divFib(depth-1, fib(depth-1)));
-        var cls = left.getAttribute('class');
-        left.setAttribute('class', `fib-left ${cls}`);
-        var leftP = document.createElement('p');
-        leftP.textContent  ='Fib(${left.depth}) = ${$left.fib}';
-        left.appendChild(leftP);
-        divFib.appendChild(left);
-
-        var right = recursiveBinTree(new divFib(depth-1, fib(depth-1)));
-        cls = right.getAttribute('class');
-        right.setAttribute('class', `fib-right ${cls}`);
-        var rightP = document.createElement('p');
-        rightP.textContent  ='Fib(${right.depth}) = ${right.fib}';
-        right.appendChild(leftP);
-        divFib.appendChild(right);
-
-        return divFib;
-*/
 var fib = function(depth){
 
     //these are my base cases
@@ -156,7 +82,7 @@ var fib = function(depth){
     div.appendChild(pDiv);
     div.appendChild(left.div);
     div.appendChild(right.div);
-    console.log(div);
+    //console.log(div);
     return {
         div: div,
         val: (left.val + right.val)
@@ -179,7 +105,6 @@ var listSlider = function(me) {
     tree.setAttribute('id', 'tree-of-divs');
     tree.setAttribute('class', 'fib-container');
 
-    //intialDiv = new divFib(me.value, fib(me.value));
     var treeObj = fib(me.value).div;
     tree.appendChild(treeObj);
 
